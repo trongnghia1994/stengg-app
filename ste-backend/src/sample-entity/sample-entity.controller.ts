93b1b02acf3b7066973eff14792673d7f04d6505
Nghia Nguyen <trongnghianguyen1994@gmail.com>
2024-04-17 08:37:06 +0700

    chore: Remove unused code

--- ste-backend/src/sample-entity/sample-entity.controller.ts
+++ ste-backend/src/sample-entity/sample-entity.controller.ts
@@ -1,9 +1,7 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
-  Param,
   Post,
   ParseIntPipe,
   Query,
@@ -17,7 +15,6 @@ import { SampleEntity } from "./sample-entity.entity";
 import { SampleEntitiesService } from "./sample-entity.service";
 import { Pagination } from "nestjs-typeorm-paginate";
 import { FileInterceptor } from "@nestjs/platform-express";
-import { FileUploadDto } from "./dto/upload-file.dto";
 
 @Controller("sample-entities")
 export class SampleEntitiesController {
@@ -28,11 +25,6 @@ export class SampleEntitiesController {
     return this.sampleEntityService.create(createSampleEntityDto);
   }
 
-  // @Get()
-  // findAll(): Promise<SampleEntity[]> {
-  //   return this.usersService.findAll();
-  // }
-
   @Get()
   findAllPaginated(
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
@@ -40,16 +32,6 @@ export class SampleEntitiesController {
     @Query('searchText') searchText: string,
   ): Promise<Pagination<SampleEntity>> {
     return this.sampleEntityService.paginate({ page, limit }, searchText);
-  }
-
-  @Get(":id")
-  findOne(@Param("id", ParseIntPipe) id: string): Promise<SampleEntity> {
-    return this.sampleEntityService.findOne(id);
-  }
-
-  @Delete(":id")
-  remove(@Param("id") id: string): Promise<void> {
-    return this.sampleEntityService.remove(id);
   }
 
   @UseInterceptors(FileInterceptor("file"))
